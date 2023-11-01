050e26d4bd522a5829521382203bbff2142e89f0
Kirill Poliak <poliak.kirill@gmail.com>
2023-11-01 17:32:57 +0200

    //Homework 2

---
+++ src/HW2/Car.java
@@ -0,0 +1,18 @@
+package HW2;
+
+public class Car {
+    public void start(){
+        startElectricity();
+        startCommand();
+        startFuelSystem();
+    }
+
+    private void startFuelSystem() {
+    }
+
+    private void startCommand() {
+    }
+
+    private void startElectricity() {
+    }
+}
